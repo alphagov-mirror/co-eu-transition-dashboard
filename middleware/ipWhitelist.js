45425874fabd4539e0018e5a6db8744b271118ac
Dominic Taylor <dom.taylor7@gmail.com>
2020-09-25 14:16:29 +0100

    use ip from x-forwarded-for header

--- middleware/ipWhitelist.js
+++ middleware/ipWhitelist.js
@@ -2,11 +2,16 @@ const config = require('config');
 const ipRangeCheck = require("ip-range-check");
 
 const ipWhitelist = (req, res, next) => {
-  if (ipRangeCheck(req.ip, config.services.tableau.whiteListedIpRange)) {
+  let ip = req.ip;
+  if(req.headers['x-forwarded-for']) {
+    ip = req.headers['x-forwarded-for'].split(',')[0];
+  }
+
+  if (ipRangeCheck(ip, config.services.tableau.whiteListedIpRange)) {
     res.locals = res.locals || {};
     res.locals.ipWhitelisted = true;
   }
   return next()
 }
 
-module.exports = ipWhitelist
+module.exports = ipWhitelist;
