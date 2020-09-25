051942a6932a3e3a3c294f63b8a62d0e5b5535b3
Dominic Taylor <dom.taylor7@gmail.com>
2020-09-25 14:23:19 +0100

    fix test

--- test/unit/middleware/ipWhitelist.test.js
+++ test/unit/middleware/ipWhitelist.test.js
@@ -5,7 +5,31 @@ const ipWhitelist = require('middleware/ipWhitelist');
 describe('middleware/ipWhitelist', () => {
   it('adds ipWhitelisted to locals on IP match', () => {
     config.services.tableau.whiteListedIpRange = ['192.168.1.1'];
-    const req = { ip: '192.168.1.1' };
+    const req = { ip: '192.168.1.1', headers: {} };
+    const res = { locals: {} };
+    const next = sinon.stub();
+
+    ipWhitelist(req, res, next)
+
+    expect(res.locals.ipWhitelisted).to.eql(true);
+    sinon.assert.calledOnce(next);
+  });
+
+  it('adds matches ip from x-forwarded-for header ', () => {
+    config.services.tableau.whiteListedIpRange = ['192.168.1.1'];
+    const req = { ip: 'no-ip', headers: { 'x-forwarded-for': '192.168.1.1' } };
+    const res = { locals: {} };
+    const next = sinon.stub();
+
+    ipWhitelist(req, res, next)
+
+    expect(res.locals.ipWhitelisted).to.eql(true);
+    sinon.assert.calledOnce(next);
+  });
+
+  it('matches first ip from x-forwarded-for header ', () => {
+    config.services.tableau.whiteListedIpRange = ['192.168.1.1'];
+    const req = { ip: 'no-ip', headers: { 'x-forwarded-for': '192.168.1.1, 192.168.1.10' } };
     const res = { locals: {} };
     const next = sinon.stub();
 
@@ -17,7 +41,7 @@ describe('middleware/ipWhitelist', () => {
 
   it('doesnt add ipWhitelisted to locals on IP mismatch', () => {
     config.services.tableau.whiteListedIpRange = ['192.168.1.100'];
-    const req = { ip: '192.168.1.1' };
+    const req = { ip: '192.168.1.1', headers: {} };
     const res = { locals: {} };
     const next = sinon.stub();
 
